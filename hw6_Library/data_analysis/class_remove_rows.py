332d075ad700e61533dbcc46aa30f070f83cc4c8
ignacuevas <93246460+ignacuevas@users.noreply.github.com>
2021-11-17 17:57:51 +0100

    changed class name

--- hw6_Library/data_analysis/class_remove_rows.py
+++ hw6_Library/data_analysis/class_remove_rows.py
@@ -4,7 +4,7 @@ Created on Wed Nov 17 09:23:27 2021
 
 @author: icuev
 """
-class Preprocessor_nan:
+class Preprocessor_Nan:
     def __init__(self,database):
         self._db=database
     def remove_nan(self,listvar):
