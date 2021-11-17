e27eef2e1d0af4bf7d0648c4219f9fbfc8178512
Ignacio Cuevas <ignacio.cuevas@bser.eu>
2021-11-17 16:01:41 +0100

    Update load and remove nan classes

--- hw6_Library/data_analysis/class_load_split_alternative.py
+++ hw6_Library/data_analysis/class_load_split_alternative.py
@@ -8,24 +8,6 @@ Created on Wed Nov 17 08:00:15 2021
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-class Load_split_data:
-    
-    def __init__(self,filename):
-        self.filename=filename
-        self._data=None
-        
-    def read_data(self):        
-        try:
-            self._data=pd.read_csv(str(self.filename))
-            return self._data
-        except FileNotFoundError:
-            return "File not found. Does the file exist?" 
-        
-    
-    def split_data(self,test_size):
-        data_train,data_test=train_test_split(self._data, test_size=test_size, random_state=42)
-        return data_train,data_test
-
 class Read_split_data:
     
     def __init__(self,filename):
@@ -35,10 +17,8 @@ class Read_split_data:
     def read_split(self,test_size):        
         try:
             self._data=pd.read_csv(str(self.filename))
+            data_train,data_test=train_test_split(self._data, test_size=test_size, random_state=42)
+            return data_train,data_test
         except FileNotFoundError:
-            return "File not found. Does the file exist?" 
-        
-        data_train,data_test=train_test_split(self._data, test_size=test_size, random_state=42)
-        
-        return data_train,data_test
+            print("File not found. Does the file exist?")
  
---
+++ hw6_Library/data_analysis/class_remove_rows.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 17 09:23:27 2021
+
+@author: icuev
+"""
+class Remove_na:
+    def __init__(self,database):
+        self._db=database
+    def remove_nan(self,listvar):
+        df=self._db.dropna(subset=listvar)
+        return df        
+        
+        
